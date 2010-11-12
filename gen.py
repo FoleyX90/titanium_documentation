faf86e3218e228d368785a253d1a8aac83927d86
Jeff Haynie <jhaynie@appcelerator.com>
2010-11-11 23:46:36 -0800

    improve gen single file

--- gen.py
+++ gen.py
@@ -41,6 +41,10 @@ if not os.path.exists(out_dir):
 	os.makedirs(out_dir)
 templates_dir = os.path.join(template_dir,'templates')
 guides = doc_json['guides']
+statics = doc_json['statics']
+languages = doc_json['languages']
+sections = []
+globals = {}
 
 def make_link(text):
 	return text.replace(' ','_').lower()
@@ -202,6 +206,7 @@ def generate_template(source,guide,language,language_code,language_dir,languages
 												languages = languages,
 												sections = sections,
 												template_dir = template_dir,
+												globals = globals,
 												description=guide['description'])
 												
 	if not os.path.exists(os.path.join(out_dir,language_dir)):
@@ -211,16 +216,90 @@ def generate_template(source,guide,language,language_code,language_dir,languages
 	f = codecs.open(file,'w',encoding='utf-8')
 	f.write(output)
 	f.close()
+
+def generate_static(static,language,language_code,language_dir,generate_if_not_found=False):
+	source = static['source']
+	file = os.path.join(template_dir,'source',language_dir,source)
+	if not os.path.exists(file) and generate_if_not_found:
+		file2 = os.path.join(template_dir,'source',source)
+		if os.path.exists(file2):
+			file = file2
+		else:
+			print "Couldn't find %s at %s or %s" % (static,file,file2)
+			print "Skipping generation..."
+			return
+	generate_template(file,static,language,language_code,language_dir,languages,sections)
+	
+def generate_guide(guide,language,language_code,language_dir,generate_if_not_found=False):	
+	source = os.path.join(template_dir,'source',language_dir,guide['source'])
+	if not os.path.exists(source) and generate_if_not_found:
+		print "Couldn't find source: %s" % source 
+		print "Generating an empty file at %s" % source
+		s = codecs.open(source,'w',encoding='utf-8')
+		X="""<summary>
+This page has not yet been developed.  Help [[contribute]] by writing documentation.
+</summary>"""
+		s.write(X)
+		s.close()
+	generate_template(source,guide,language,language_code,language_dir,languages,sections)
+
+def get_language(lang):
+	for language_entry in languages:
+		language = language_entry['title']
+		language_dir = language_entry['directory']
+		language_code = language_entry['code']
+		if language_code == lang:
+			return language,language_dir,language_code
+	print "Couldn't find language: %s" % lang
+	sys.exit(1)
 	
 def main(args):
 	if os.path.exists(os.path.join(out_dir,'assets')):
 		shutil.rmtree(os.path.join(out_dir,'assets'))
 	shutil.copytree(os.path.join(template_dir,'assets'),os.path.join(out_dir,'assets'))
 	
-	languages = doc_json['languages']
-	sections = []
+	
+	# add any key=value pair as a global so that they can be picked up in template
+	for arg in args:
+		i = arg.find('=')
+		if i == -1: continue
+		k,v = arg.split('=')
+		globals[k]=v
 
-	for entry in doc_json['guides']:
+	lang = 'en'
+	if globals.has_key('lang'):
+		lang = globals['lang']
+	language, language_code, language_dir = get_language(lang)
+
+	if globals.has_key('guide'):
+		found = False
+		guide = globals['guide']
+		for entry in doc_json['guides']:
+			if entry['source']==guide:
+				found = True
+				break
+		if not found:
+			print "No guide found: %s" % guide
+			sys.exit(1)
+		else:
+			generate_guide(entry,language,language_code,language_dir)
+			sys.exit(0)
+
+	if globals.has_key('static'):
+		found = False
+		static = globals['static']
+		for entry in doc_json['statics']:
+			if entry['source']==static:
+				found = True
+				break
+		if not found:
+			print "No static entry found: %s" % static
+			sys.exit(1)
+		else:
+			generate_static(entry,language,language_code,language_dir)
+			sys.exit(0)
+
+	for entry in guides:
 		section = entry['section']
 		
 		found = False
@@ -243,31 +322,11 @@ def main(args):
 			print "No language directory found at: %s" % os.path.join(template_dir,'source',language_dir)
 			continue
 
-		for static_entry in doc_json['statics']:
-			static = static_entry['source']
-			file = os.path.join(template_dir,'source',language_dir,static)
-			if not os.path.exists(file):
-				file2 = os.path.join(template_dir,'source',static)
-				if os.path.exists(file2):
-					file = file2
-				else:
-					print "Couldn't find %s at %s or %s" % (static,file,file2)
-					print "Skipping generation..."
-					continue
-			generate_template(file,static_entry,language,language_code,language_dir,languages,sections)
+		for static in statics:
+			generate_static(static,language,language_code,language_dir,True)
 	
 		for guide in guides:
-			source = os.path.join(template_dir,'source',language_dir,guide['source'])
-			if not os.path.exists(source):
-				print "Couldn't find source: %s" % source 
-				print "Generating an empty file at %s" % source
-				s = codecs.open(source,'w',encoding='utf-8')
-				X="""<summary>
-	This page has not yet been developed.  Help [[contribute]] by writing documentation.
-</summary>"""
-				s.write(X)
-				s.close()
-			generate_template(source,guide,language,language_code,language_dir,languages,sections)
+			generate_guide(guide,language,language_code,language_dir,True)
 	
 
 if __name__ == '__main__':
